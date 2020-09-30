b25563dc2a3043b8529bf0b2464e23c60d5a0514
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-29 21:25:08 -0400

    Add logic to the Appointment component and two stories to check if the Show and Empty components are working fine with the Appointment component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 import './styles.scss'
 
-const Appointment = () => {
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+
+const Appointment = props => {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        < Show student={props.interview.student} interviewer={props.interview.interviewer} /> :
+        < Empty />}
+    </article>
   )
 };
 
