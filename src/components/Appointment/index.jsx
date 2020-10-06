117a690e13bd1fd340293d6fc28c87d2376961e9
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-06 17:34:09 -0400

    Add another Application test to simulate a user creating a new appointment. To do so, we had included a testid to our Appointment component and had added a new function to our axios mock library

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -49,7 +49,7 @@ const Appointment = props => {
   };
 
   return (
-    <article className="appointment">
+    <article data-testid="appointment" className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && (
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -15,22 +15,59 @@ afterEach(cleanup);
 //     expect(getByText("Leopold Silvers")).toBeInTheDocument();
 //   });
 // });
+describe("Application", () => {
 
-// using async/await:
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-  await waitForElement(() => getByText("Monday"));
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+
+
+  // using async/await:
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // Render the Application
+    const { container } = render(<Application />);
+
+    // Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // search for all appointments in the original container by querying the test id
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    // to test that the saving works we need to click the add button, change the student name input, select an interviewer and click the save button.
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    })
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // Wait until the text "Lydia Miller-Jones" is displayed in out appointment element.
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+
+
+  });
+
+
+})
+
 
 /*
-  About the Application Test Suite:
-    We will mock the functions we use from the axios library.
-    We will write a test to confirm that the scheduler can load data.
-    We will write an asynchronous test that waits for a component to update before proceeding.
-    We will use containers to find specific DOM nodes.
-    We will chain promises to handle asynchronous testing.
-    We will override mock implementations for specific tests.
-    We will use setup and teardown functions provided by Jest to perform common tasks.
+About the Application Test Suite:
+  We will mock the functions we use from the axios library.
+  We will write a test to confirm that the scheduler can load data.
+  We will write an asynchronous test that waits for a component to update before proceeding.
+  We will use containers to find specific DOM nodes.
+  We will chain promises to handle asynchronous testing.
+  We will override mock implementations for specific tests.
+  We will use setup and teardown functions provided by Jest to perform common tasks.
 */
